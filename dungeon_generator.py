176d4df5e6fb2949f1623047aac7fefadea46681
Benjamin <benjaminsei12@gmail.com>
2021-11-28 18:46:10 -0500

    forgot to push ms1 and readme

--- dungeon_generator.py
+++ dungeon_generator.py
@@ -1,7 +1,8 @@
 import random
-from collections import deque
 from enum import Enum
-from typing import List
+from collections import deque
+from collections import defaultdict
+from typing import Dict, List, Set, Tuple
 
 class Direction(Enum):
     LEFT = 1
@@ -17,19 +18,35 @@ def opposite(direction: Direction):
         case Direction.DOWN: return Direction.UP
 
 class CC():
-    def __init__(self, rooms, idx):
-        self.rooms, self.idx = rooms, idx
-        self.adj = set()
+    def __init__(self, rooms: Set['Room'], idx: int):
+        self.rooms: Set[Room] = rooms
+        self.idx: int = idx
+        self.adj: Dict[CC, Set[int]] = defaultdict(lambda: set())
+        self.parent: CC | None = None
+        self.artifacts: Set[int] = set()
+    
+    def get_path(self) -> List['CC']:
+        curr = self
+        path = deque()
+        while curr is not None:
+            path.appendleft(curr)
+            curr = curr.parent
+        return list(path)
 
 class Corridor():
-    def __init__(self, r1, r2, type):
-        self.rooms, self.type = (r1, r2), type
+    def __init__(self, r1: 'Room', r2: 'Room', type: int):
+        self.rooms: Tuple[Room, Room] = (r1, r2) 
+        self.type: int = type
 
 class Room():
     def __init__(self):
-        self.left, self.right, self.up, self.down = False, False, False, False
-        self.corridors = {}
-        self.cc: int | None = None
+        self.left: bool = False
+        self.right: bool = False
+        self.up: bool = False
+        self.down: bool = False
+        self.corridors: Dict[Direction, Corridor] = {}
+        self.cc: int = None #type: ignore
+        self.artifact: int | None = None
 
     def __getitem__(self, index: Direction):
         match index:
@@ -53,8 +70,14 @@ class Room():
             case Direction.DOWN:
                 self.down = value
 
-    def __str__(self):
+    def __repr__(self):
         return "(" + " ".join([direction.name if self[direction] else "XXXX" for direction in Direction]) + ")"
+    
+    def get_txt_repr(self):
+        top = f"x{self.corridors[Direction.UP].type if self.up else 'x'}x "
+        mid = f"{self.corridors[Direction.LEFT].type if self.left else 'x'}{self.artifact if self.artifact is not None else '_'}{self.corridors[Direction.RIGHT].type if self.right else 'x'} "
+        bot = f"x{self.corridors[Direction.DOWN].type if self.down else 'x'}x "
+        return top, mid, bot
 
 class Dungeon():
     '''
@@ -64,30 +87,35 @@ class Dungeon():
     p_door, probability of placing a door
     num_types, number of obstacle types
     '''
-    def __init__(self, w, h, p_corridor, p_door, num_types):
-        assert(w > 3 < h)
-        assert(0 <= p_corridor < 1 and 0 <= p_door < 1)
+    def __init__(self, w: int, h: int, p_corridor: int | float, p_door: int | float, num_types: int):
+        assert(w >= 3 <= h)
+        assert(0 <= p_corridor <= 1 and 0 <= p_door <= 1)
         assert(num_types > 0)
-        self.w, self.h, self.p_corridor, self.p_door, self.num_types = w, h, p_corridor, p_door, num_types
-        self.start = (h - 1, random.randint(0, w - 1))
-        self.end = None
-        self.board: List[List[Room | None]] = [[None] * w for _ in range(h)]
-        self.corridors = set()
-        self.obstacles = set()
-        self.ccs = []
-        self.start_cc_idx = None
-        self.end_cc_idx = None
+        self.w: int = w
+        self.h: int = h
+        self.p_corridor: float | int = p_corridor
+        self.p_door: float | int = p_door
+        self.num_types: int = num_types
+        self.start: Tuple[int, int] = (h - 1, random.randint(0, w - 1))
+        self.end: Tuple[int, int] = None #type: ignore
+        self.board: List[List[Room]] = [[None] * w for _ in range(h)] #type: ignore
+        self.corridors: Set[Corridor] = set()
+        self.obstacles: Set[Corridor] = set()
+        self.ccs: List[CC] = []
+        self.start_cc_idx: int = None #type: ignore
+        self.end_cc_idx: int = None #type: ignore
         self.build_solution()
         self.fill_empty_rooms()
         self.fix_border_rooms()
         self.insert_corridors()
+        self.find_connected_components()
         self.place_artifacts()
 
     def build_solution(self):
         r, c = self.start
         room = self.board[r][c] = Room()
         room[Direction.LEFT], room[Direction.RIGHT] = True, True
-        while (self.end is not None):
+        while (self.end is None):
             exits = [Direction.UP]
             if c != 0 and self.board[r][c - 1] is None:
                 exits.append(Direction.LEFT)
@@ -100,7 +128,7 @@ class Dungeon():
                 match direction:
                     case Direction.UP:
                         room[Direction.UP] = True
-                        r += 1
+                        r -= 1
                     case Direction.LEFT: c -= 1
                     case Direction.RIGHT: c += 1
                 room = self.board[r][c] = Room()
@@ -113,23 +141,19 @@ class Dungeon():
                 if (self.board[r][c] is not None): continue
                 room = self.board[r][c] = Room()
                 for direction in Direction:
-                    if adj := self.get_adj_room(r, c, direction):
-                        room[opposite(direction)] = True
+                    if (adj := self.get_adj_room(r, c, direction)) and adj[opposite(direction)]:
+                        room[direction] = True
                     if not room[direction] and adj is None:
                         room[direction] = random.random() < self.p_corridor
 
     def fix_border_rooms(self):
         for room in self.board[0]:
-            assert(room is not None)
             room[Direction.UP] = False
         for room in self.board[-1]:
-            assert(room is not None)
             room[Direction.DOWN] = False
         for row in self.board:
-            assert(row[0] is not None)
             row[0][Direction.LEFT] = False
-            assert((end := row[-1]) is not None)
-            end[Direction.RIGHT] = False
+            row[-1][Direction.RIGHT] = False
 
     '''
     Insert corridors, where a corridor is either obstacle-free (type = 0) or contains an obstacle (type > 0)
@@ -138,7 +162,6 @@ class Dungeon():
         for r in range(self.h):
             for c in range(self.w):
                 room = self.board[r][c]
-                assert(room is not None)
                 for direction in (Direction.DOWN, Direction.RIGHT):
                     if room[direction]:
                         adj = self.get_adj_room(r, c, direction)
@@ -150,41 +173,66 @@ class Dungeon():
                         if corridor.type > 0:
                             self.obstacles.add(corridor)
 
-    def place_artifacts(self):
+    def find_connected_components(self):
         idx = 0
-        visited = set()
-        for r in range(self.h):
-            for c in range(self.w):
-                if (r, c) in visited: continue
-                cc = {(r, c)}
-                visited.add((r, c))
-                queue = deque([(r, c)])
+        visited: Set[Tuple[int, int]] = set()
+        for _r in range(self.h):
+            for _c in range(self.w):
+                if (_r, _c) == (1, 2):
+                    pass
+                if (_r, _c) in visited: continue
+                cc = set()
+                visited.add((_r, _c))
+                queue = deque([(_r, _c)])
                 while queue:
                     r, c = queue.popleft()
                     room = self.board[r][c]
-                    assert(room is not None)
                     room.cc = idx
+                    cc.add((r, c))
                     for direction, corridor in room.corridors.items():
                         if corridor.type > 0: continue
-                        adj = self.get_adj_room(r, c, direction)
-                        if adj not in visited:
-                            visited.add(adj)
-                            queue.append(self.get_adj_coords(r, c, direction))
+                        nxt = self.get_adj_coords(r, c, direction)
+                        if nxt not in visited:
+                            visited.add(nxt)
+                            queue.append(nxt)
                 if self.start in cc: self.start_cc_idx = idx
                 if self.end in cc: self.end_cc_idx = idx
                 self.ccs.append(CC(cc, idx))
                 idx += 1
+        assert(len(visited) == self.w * self.h)
         for obstacle in self.obstacles:
-            cc_1, cc_2 = [self.ccs[r.cc] for r in obstacle.rooms]
-            cc_1.adj.add((cc_2, obstacle.type))
-            cc_2.adj.add((cc_1, obstacle.type))
-        #TODO: run BFS on graph of CCs to find shortest path from
-        # source cc to destination cc and  place corresponding artifacts
+            cc_1, cc_2 = [self.ccs[room.cc] for room in obstacle.rooms]
+            cc_1.adj[cc_2].add(obstacle.type)
+            cc_2.adj[cc_1].add(obstacle.type)
 
-    def in_bounds(self, r, c):
+    def place_artifacts(self):
+        curr_cc = self.ccs[self.start_cc_idx]
+        visited = {curr_cc}
+        queue = deque([curr_cc])
+        while queue:
+            curr_cc = queue.popleft()
+            for neighbor in curr_cc.adj:
+                if neighbor in visited: continue
+                neighbor.parent = curr_cc
+                visited.add(neighbor)
+                queue.append(neighbor)
+        path = self.ccs[self.end_cc_idx].get_path()
+        assert(path[0].idx == self.start_cc_idx)
+        placed_keys: Set[int] = set()
+        for cc1, cc2 in zip(path, path[1:]):
+            if cc1.adj[cc2].intersection(placed_keys): continue
+            artifact = random.choice(list(cc1.adj[cc2]))
+            placed_keys.add(artifact)
+            cc1.artifacts.add(artifact)
+            r, c = random.choice(list(cc1.rooms))
+            room = self.board[r][c]
+            room.artifact = artifact
+        # TODO: Add more artifacts to the graph that have not been placed yet
+
+    def in_bounds(self, r: int, c: int):
         return 0 <= r < self.h and 0 <= c < self.w
     
-    def get_adj_room(self, r, c, direction: Direction):
+    def get_adj_room(self, r: int, c: int, direction: Direction):
         match direction:
             case Direction.LEFT: c -= 1
             case Direction.RIGHT: c += 1
@@ -192,7 +240,7 @@ class Dungeon():
             case Direction.DOWN: r += 1
         return False if not self.in_bounds(r, c) else self.board[r][c]
 
-    def get_adj_coords(self, r, c, direction: Direction):
+    def get_adj_coords(self, r: int, c: int, direction: Direction):
         match direction:
             case Direction.LEFT: c -= 1
             case Direction.RIGHT: c += 1
@@ -200,5 +248,15 @@ class Dungeon():
             case Direction.DOWN: r += 1
         return (r, c)
 
-    # def __str__(self):
-    #     ...
+def gen_txt(dungeon: Dungeon, filename: str):
+    with open(filename, 'w') as f:
+        for row in dungeon.board:
+            for line in zip(*(room.get_txt_repr() for room in row)):
+                f.write("".join(line) + '\n')
+            f.write('\n')
+
+if __name__ == '__main__':
+    random.seed(1)
+    dungeon = Dungeon(4, 4, 0.0, 0.5, 9)
+    gen_txt(dungeon, 'dungeon.txt')
+    print('yay')
