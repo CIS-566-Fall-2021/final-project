93aee5b08bb13cfb85fbe2bd4c238a5f2e67527c
rchu00 <ruth.i.chung@gmail.com>
2021-12-06 18:00:39 -0500

    functional webgl render

---
+++ src/shaders/instanced-vert.glsl
@@ -0,0 +1,23 @@
+#version 300 es
+
+uniform mat4 u_ViewProj;
+uniform float u_Time;
+
+uniform mat3 u_CameraAxes; // Used for rendering particles as billboards (quads that are always looking at the camera)
+// gl_Position = center + vs_Pos.x * camRight + vs_Pos.y * camUp;
+
+in vec4 vs_Pos; // Non-instanced; each particle is the same quad drawn in a different place
+in vec4 vs_Nor; // Non-instanced, and presently unused
+in vec4 vs_Col; // An instanced rendering attribute; each particle instance has a different color
+in vec3 vs_Translate; // Another instance rendering attribute used to position each quad instance in the scene
+in vec2 vs_UV; // Non-instanced, and presently unused in main(). Feel free to use it for your meshes.
+
+out vec4 fs_Col;
+out vec4 fs_Pos;
+out vec2 fs_UV;
+
+void main()
+{
+    fs_UV = vs_UV;
+    gl_Position = u_ViewProj * vec4(vec3(vs_Pos) + vs_Translate, 1.);
+}
